78a890557b20b1fc9a44430a643697a7e558f5f0
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-14 21:15:52 -0400

    #007 - Created an Interface for the navbar content list

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+interface NavLink {
+  label: string;
+  href: string;
+}
+
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.html',
   styleUrl: './nav.scss',
 })
+
 export class Nav {}
