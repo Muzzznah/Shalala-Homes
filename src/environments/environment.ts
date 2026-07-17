3d05d8acc34a8e64578fec76797451c1c4311499
heart <xdxd@gmail.com>
2026-07-17 14:42:48 -0400

    create supabase service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://rraoktjambzannqgrrvi.supabase.co',
   supabaseKey: 'sb_publishable_lNQ8WW8dhMh2Z9yVwv3O1w_j4ua9IUB',
 };
