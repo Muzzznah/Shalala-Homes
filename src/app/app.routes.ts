226e2d5f5d21e79d16660c298df8e1fe23b57f8a
heart <xdxd@gmail.com>
2026-07-19 20:45:41 -0400

    add admin login form, add login and admin rental routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Rentals } from './components/rentals/rentals';
+import { AdminLogin } from './components/admin-login/admin-login';
+import { AdminRentals } from './components/admin-rentals/admin-rentals';
+import { authGuard } from './core/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'rentals',
+    component: Rentals,
+  },
+  {
+    path: 'admin/login',
+    component: AdminLogin,
+  },
+  {
+    path: 'admin/rentals',
+    component: AdminRentals,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
